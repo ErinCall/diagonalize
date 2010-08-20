d23a0604ad530533dcde6a4c30b81be5adbc60d2
James Rucker <git@jahgru.com>
2010-08-20 12:57:53 +0800

    Fixed Andrew's god awful indentation.

--- diagonalize.pl
+++ diagonalize.pl
@@ -7,7 +7,7 @@ local $/;
 use Getopt::Long;
 
 GetOptions(
-	'c|code-tags' => \my $code_tags,
+    'c|code-tags' => \my $code_tags,
 );
 
 my $filename = shift;
@@ -17,14 +17,13 @@ print "Enter text\n";
 my $str = <STDIN>;
 my @words = split /\s/, $str;
 for my $i ( 0 .. $#words) {
-		  my $word = $words[$i];
-		  my @word = split '', $word;
-		  for my $l ( 0 .. $#word){
-					 $words[$i] = ref $words[$i] ? $words[$i] : [];
-					 push @{$words[$i]}, (' ' x $l) . $word[$l];
-		  }
+    my $word = $words[$i];
+    my @word = split '', $word;
+    for my $l ( 0 .. $#word){
+        $words[$i] = ref $words[$i] ? $words[$i] : [];
+        push @{$words[$i]}, (' ' x $l) . $word[$l];
+    }
 }
 print '[code]' if $code_tags;
 map { map {print $_; print "\n"} @$_; } @words;
 print "[/code]\n" if $code_tags;
-
